c596d02ca1c8590cf8c4c7a79567a4d24c7af5ea
Steve Tuckner <stevetuckner@gmail.com>
2016-10-14 17:08:44 -0500

    Add in things controller and models for something that is stored in the database

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :things
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "things#index"
 end
+
